5be01df2f1caf97bf490a24ac61df5f2741d99c1
NotFound1911 <465442062@qq.com>
2023-12-17 11:17:12 +0800

    [test]补充测试

--- client_e2e_test.go
+++ client_e2e_test.go
@@ -14,7 +14,7 @@ import (
 // go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 // cd internal/proto
 // protoc --go_out=. user.proto
-func TestInitClientProxy(t *testing.T) {
+func TestInitServiceProto(t *testing.T) {
 	server := NewServer()
 	service := &UserServiceServer{}
 	server.RegisterService(service)
@@ -83,6 +83,72 @@ func TestInitClientProxy(t *testing.T) {
 			if resp != nil && resp.User != nil {
 				assert.Equal(t, tc.wantResp.Msg, resp.User.Name)
 			}
+		})
+	}
+}
+
+func TestInitClientProxy(t *testing.T) {
+	server := NewServer()
+	service := &UserServiceServer{}
+	server.RegisterService(service)
+	go func() {
+		err := server.Start("tcp", ":8081")
+		t.Log("err:", err)
+	}()
+	time.Sleep(time.Second * 3)
+	usClient := &UserService{}        // 客户端服务
+	client, err := NewClient(":8081") // json 协议
+	require.NoError(t, err)
+	err = client.InitService(usClient)
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name string
+		mock func()
+
+		wantErr  error
+		wantResp *GetByIdResp
+	}{
+		{
+			name: "no error",
+			mock: func() {
+				service.Err = nil
+				service.Msg = "hello world"
+			},
+			wantResp: &GetByIdResp{
+				Msg: "hello world",
+			},
+		},
+		{
+			name: "error",
+			mock: func() {
+				service.Msg = ""
+				service.Err = errors.New("test error")
+			},
+			wantResp: &GetByIdResp{},
+			wantErr:  errors.New("test error"),
+		},
+		{
+			name: "both",
+			mock: func() {
+				service.Msg = "hello world"
+				service.Err = errors.New("test error")
+			},
+			wantResp: &GetByIdResp{
+				Msg: "hello world",
+			},
+			wantErr: errors.New("test error"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+			resp, err := usClient.GetById(context.Background(), &GetByIdReq{
+				Id: 123,
+			})
+			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantResp, resp)
 		})
 	}
 }
